93712efd2bb725aa1e71b784e12104efed0f5d50
Jonas Schlemmer <jonas.schlemmer@gmail.com>
2026-02-04 11:49:21 -0300

    Várias modificações
    - Adicionado ícone para subir página.
    - Criado rodapé.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # henrich-advocacia
 
 (pegar ref em outros: artc)
+
+https://jonas-schlemmer.github.io/henrich-advocacia/
--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,35 @@ document.addEventListener('contextmenu', e => e.preventDefault());
 
 // Bloquear drag
 document.addEventListener('dragstart', e => e.preventDefault());
+
+// Ajustar velocidade do vídeo para 1.5x
+window.addEventListener('load', () => {
+    const video = document.getElementById('logo-video');
+    if (video) {
+        video.playbackRate = 1.5;
+    }
+});
+
+
+
+
+
+
+
+
+const btnTopo = document.getElementById("btn-topo");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        btnTopo.style.display = 'flex'
+    } else {
+        btnTopo.style.display = 'none'
+    }
+  });
+
+  btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
